e3f54dd9faf64c4825fff1810978155abe44611c
Jeesun Kim <jeesunikim@gmail.com>
2016-08-13 21:01:49 -0400

    stylign for sticky nav update

--- _assets/_js/nav.js
+++ _assets/_js/nav.js
@@ -7,10 +7,11 @@
 	fixedNav.prototype.init = function () {
 
 		this.isSticky = false;
-		this.mainNav = $('header');
+		this.header = $('header');
+		this.mainNav = this.header.find('nav.site-nav');
 		this.bodyContainer = $('body');
 		this.scrollPositionStart = 0;
-		this.defaultLH = parseInt(this.mainNav.css('line-height'));
+		this.defaultLH = parseInt(this.header.css('line-height'));
 		
 		navSelf = this;
 
@@ -29,7 +30,7 @@
 
 	    if (!navSelf.isSticky) {
 
-	        this.currentLH = parseInt(navSelf.mainNav.css('line-height'));
+	        this.currentLH = parseInt(navSelf.header.css('line-height'));
 	        this.calculatedLH;
 
 			// console.log(this.defaultLH, 'this.defaultLH');
@@ -43,41 +44,28 @@
 	        	 this.calculatedLH = navSelf.defaultLH - scrollTop;
 
 
-        		navSelf.mainNav.css('line-height', this.calculatedLH + 'px'); 	
-
-	        	console.log(navSelf.defaultLH, 'navSelf.defaultLH', this.currentLH, 'this.currentLH')       
+        		navSelf.header.css('line-height', this.calculatedLH + 'px'); 	
+       
 	        	if ( this.currentLH < 92 ) {
-		        	console.log("HEYY")
-		        	navSelf.mainNav.css('line-height', 88 + 'px'); 
-		        } 
+		        	this.setSticky();
+		        }  else {
+		        	this.setUnsticky();
+		        }
 
 	        } else {
 	        	this.calculatedLH = navSelf.defaultLH + scrollTop;
 
-        		navSelf.mainNav.css('line-height', this.calculatedLH + 'px'); 	
+        		navSelf.header.css('line-height', this.calculatedLH + 'px'); 
+        		
+        		if ( this.currentLH > 92 ) {
+		        	this.setUnsticky();
+		        } 	
 	        }
 
 	        
 
 	    }
 
-	    // console.log(scrollTop, 'scrollTop', this.defaultLH, 'defaultLH');
-
-	    // subtract the number of scroll value 
-
-	    // var isMobile = (this.breakpoints.current === 'sm') ? true : false;
-	    // var mainNavFixed = $(this.mainNav).hasClass('is-fixed');
-
-	    // var isNavFixed = mainNavFixed && isMobile;
-	    // var navAdjustment = isNavFixed ? this.mainNavHeight : 0;
-	    // var threshold = mainNavFixed && isMobile ? this.originalY - this.mainNavHeight : this.originalY;
-
-	    // // check if we need to set sticky
-	    // if (scrollTop >= threshold && !this.isSticky) {
-	    //     this.setSticky();
-	    // } else if (scrollTop < (this.originalY - navAdjustment) && this.isSticky) {
-	    //     this.setUnsticky();
-	    // }
 	};
 
 	fixedNav.prototype.startScrolling = function () {
@@ -91,11 +79,14 @@
 
 	fixedNav.prototype.setSticky = function () {
 		navSelf.isSticky = true;
+		// navSelf.header.css('line-height', 88 + 'px'); 
+		navSelf.mainNav.addClass('fixed')
 
 	}
 
 	fixedNav.prototype.setUnsticky = function () {
 		navSelf.isSticky = false;
+		navSelf.mainNav.removeClass('fixed');
 	}
 
 	$(function() {
