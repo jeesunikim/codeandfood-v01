3657755e6c8cca1d1650bb828a40b7aba73e771d
jeesunikim <jeesunikim@gmail.com>
2019-05-04 22:38:38 -0400

    bugfix/async_gist_onrefresh > added asyncGist in page refresh

--- _assets/_js/index.js
+++ _assets/_js/index.js
@@ -1,6 +1,7 @@
 import "./../_scss/style.scss";
 import StickyNav from "./components/stickyNav";
 import PageAnimation from "./components/pageAnimation";
+import AsyncGist from "./components/asyncGist";
 
 class CodeandFood {
     constructor() {
@@ -11,6 +12,22 @@ class CodeandFood {
 
         StickyNavInstance.init();
         PageAnimationInstance.init();
+        this.loadGists();
+    }
+
+    loadGists() {
+        const gistEls = document.querySelectorAll("code[data-type='gist']");
+
+        if (gistEls) {
+            gistEls.forEach(el => {
+                const projectContainer = el.closest(".project-image");
+                if (el.attributes.gist) {
+                    const gistURL = el.attributes.gist.value;
+                    const AsyncGistInstance = new AsyncGist();
+                    AsyncGistInstance.loadContent(gistURL, projectContainer);
+                }
+            });
+        }
     }
 }
 
